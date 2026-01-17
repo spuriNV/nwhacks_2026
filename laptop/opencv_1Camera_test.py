206b530648655cac135a90232bbd73a6b304fe24
Hayden Mai <144507076+haydenmai@users.noreply.github.com>
2026-01-17 14:32:25 -0800

    Sample opencv code

---
+++ laptop/opencv_1Camera_test.py
@@ -0,0 +1,31 @@
+import cv2
+
+# Replace with your Windows GStreamer bin path if needed
+pipeline = (
+    'udpsrc port=5000 caps="application/x-rtp,media=video,encoding-name=JPEG,payload=26" ! '
+    'rtpjpegdepay ! jpegdec ! videoconvert ! appsink'
+)
+
+cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
+if not cap.isOpened():
+    raise RuntimeError("Failed to open video stream")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Flip upside down if needed
+    # frame = cv2.flip(frame, -1)  # -1 flips both vertically and horizontally
+
+    # Example processing: convert to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Show the frame
+    cv2.imshow("Camera 1", gray)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ pi_stream.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# USEFUL COMMANDS
+# v4l2-ctl --list-devices
+# sudo ldconfig && rpicam-hello --list-cameras
+# https://medium.com/@sepideh.92sh/setup-and-troubleshooting-of-raspberry-pi-camera-module-v2-1-imx219-on-ubuntu-24-04-lts-fb518f4576c0
+
+LAPTOP_IP="192.168.137.1"
+
+# CSI Camera 1
+rpicam-vid --camera 0 -t 0 --width 640 --height 480 --framerate 30 \
+  --codec mjpeg --quality 70 -n -o - | \
+  gst-launch-1.0 fdsrc ! jpegparse ! rtpjpegpay ! \
+  udpsink host=$LAPTOP_IP port=5000 &
+
+# CSI Camera 2
+rpicam-vid --camera 1 -t 0 --width 640 --height 480 --framerate 30 \
+  --codec mjpeg --quality 70 -n -o - | \
+  gst-launch-1.0 fdsrc ! jpegparse ! rtpjpegpay ! \
+  udpsink host=$LAPTOP_IP port=5001 &
+
+# USB Camera 3 (Change /dev/video0 if needed)
+gst-launch-1.0 v4l2src device=/dev/video16 ! \
+    video/x-raw,width=640,height=480 ! \
+    videoconvert ! jpegenc quality=70 ! rtpjpegpay ! udpsink host=$LAPTOP_IP port=5002 &
+
+wait
