bb3508a53c9f68df523a6909a463133cc03b085c
wwwsb <Jonathanliu865@gmail.com>
2026-01-17 12:47:33 -0800

    Implemented basic set up, added functions for distance and vibration level setting

--- arduino/sketch_jan17a/sketch_jan17a.ino
+++ arduino/sketch_jan17a/sketch_jan17a.ino
@@ -15,11 +15,51 @@ const int levelThresholds[3] = {33, 66, 100};
 const int vibrationPWM[4] = {0, 85, 170, 255};
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
 
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    pinMode(trigPins[i], OUTPUT);
+    pinMode(echoPins[i], INPUT);
+
+    digitalWrite(trigPins[i], LOW);
+    //analogWrite(motorPins[i], 0);
+    digitalWrite(BUZZER_PIN, LOW);
+  }
+  //pinMode(motorPins[0], OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW); // start OFF
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+
+
+    long distance = getDistanceCM(trigPins[i], echoPins[i]);
+    int level = getVibrationLevel(distance);
+
+    //analogWrite(motorPins[i], vibrationPWM[level]);
+    //correct one for vibration
+}
+
+long getDistanceCM(int trigPin, int echoPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH, 30000);
+  if (duration == 0) return -1;
+
+  return duration * 0.034 / 2;
+}
+
+int getVibrationLevel(long distance) {
+  if (distance == -1 || distance > 100) return 0;
+  else if (distance > 66) return 1;
+  else if (distance > 33) return 2;
+  else return 3;
+}
 
 }
